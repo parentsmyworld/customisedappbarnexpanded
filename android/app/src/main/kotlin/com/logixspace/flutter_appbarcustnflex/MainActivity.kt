338b33de6996314e8c9b772f9a263c55c9788204
parentsmyworld <madhu@skylinkmsc.com>
2020-09-23 16:30:43 +0300

    appbarcustomisedandexpanded

---
+++ android/app/src/main/kotlin/com/logixspace/flutter_appbarcustnflex/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.logixspace.flutter_appbarcustnflex
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          title:
+          Center(child: Text("Appbar and Expanded",style: TextStyle(
+            color: Colors.black
+          ),)),
+          backgroundColor: Colors.purple,
+          leading: Icon(Icons.menu,
+          color: Colors.black,),
+          actions: [
+            Icon(Icons.search,
+                color: Colors.black),
+            Icon(Icons.account_circle,
+                color: Colors.black)
+          ],
+        ),
+        body: Container(
+          padding: EdgeInsets.all(30),
+          child:
+//              Row(
+//                children: [
+//                  Expanded(
+//                    flex: 1,
+//                    child: Container(
+//                        padding: EdgeInsets.all(30),
+//                        color: Colors.amber,
+//                        child: Center(child: Text("Hello"))),
+//                  ),
+//                  Expanded(
+//                    flex: 1,
+//                    child: Container(
+//                        padding: EdgeInsets.all(30),
+//                        color: Colors.yellowAccent,
+//                        child: Center(child: Text("World"))),
+//                  ),
+//                  Expanded(
+//                    flex: 1,
+//                    child: Container(
+//                        padding: EdgeInsets.all(30),
+//                        color: Colors.greenAccent,
+//                        child: Center(child: Text("!"))),
+//                  )
+//
+//                ],
+//              ),
+                  Column(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+//                        padding: EdgeInsets.all(30),
+                            color: Colors.amber,
+                            child: Center(child: Text("Hello"))),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+//                        padding: EdgeInsets.all(30),
+                            color: Colors.yellowAccent,
+                            child: Center(child: Text("World"))),
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Container(
+//                        padding: EdgeInsets.all(30),
+                            color: Colors.greenAccent,
+                            child: Center(child: Text("!"))),
+                      )
+                    ],
+                  ),
+
+
+              )
+          ),
+
+    );
+  }
+}
